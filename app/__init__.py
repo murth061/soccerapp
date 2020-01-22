5d667a7bef7d66777efd1fbe4e2c3972c6c7f22e
Suriya Murthy <murth061@umn.edu>
2020-01-22 09:27:13 -0600

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
+from flask_bootstrap import Bootstrap
+import requests
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+bootstrap = Bootstrap(app)
+
+from app import routes, models
